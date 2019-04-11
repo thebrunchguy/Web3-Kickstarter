fd2e41d002c2466a2cd8e8a2f901d8089338be3e
David Kobrosky <david.kobrosky@vmny-david.kobrosky>
2019-04-11 13:07:02 -0400

    Full test added

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -76,6 +76,35 @@ describe('Campaigns', () => {
       const request = await campaign.methods.requests(0).call();
 
       assert.equal('Buy bat', request.description);
+  });
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    })
+
+    await campaign.methods
+      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({from:accounts[0], gas: '1000000'});
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+    console.log(balance)
+
+    assert(balance > 104);
   })
+
 
 });
