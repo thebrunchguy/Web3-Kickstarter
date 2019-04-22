f6f610916a6876c56e027ef35bcf101de48a45c5
David Kobrosky <david.kobrosky@vmny-david.kobrosky>
2019-04-22 15:25:10 -0400

    added layout

---
+++ components/layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//campaign list should be a child of layout
+export default () => {
+  return (
+    <div>
+      <h1> I'm a header </h1>
+
+      <h1> I'm a footer </h1>
+    </div>
+
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ class CampaignIndex extends Component {
       <link
       rel="stylesheet"
       href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+      <h3> Open campaigns </h3>
       {this.renderCampaigns()}
       <Button
         content= "Create Campaign"
